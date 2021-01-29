b913ee6f566f0f55d75744a92a473f6835e203d3
prabhat2020 <kprabhat0123@outlook.com>
2021-01-29 16:47:13 +0530

    add test cases

--- src/test/kotlin/com/tcs/service/component/AuthTest.kt
+++ src/test/kotlin/com/tcs/service/component/AuthTest.kt
@@ -1,18 +1,13 @@
 package com.tcs.service.component
 
-import com.tcs.service.constant.URLPath
+import com.tcs.service.events.ECMRPostedEvent
 import com.tcs.service.events.PostEcmrEvent
-import com.tcs.service.model.ContainerInShipItems
-import com.tcs.service.model.ECMR
-import com.tcs.service.model.OrderInShipment
-import com.tcs.service.model.ShipUnit
+import com.tcs.service.model.*
 import com.tcs.service.service.Auth
-import com.tcs.service.utility.Utility
+import com.tcs.service.service.UpdateStatusFlag
 import org.junit.Assert
 import org.junit.Test
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.client.RestTemplate
-import java.util.stream.Collectors
 
 class AuthTest {
 
@@ -21,6 +16,8 @@ class AuthTest {
 var restTemplate: RestTemplate = RestTemplate()
 
     var auth: Auth = Auth(restTemplate)
+
+    var updateStatus: UpdateStatusFlag= UpdateStatusFlag(restTemplate,auth)
 
     @Test
     fun prepareAuthTest() {
@@ -37,5 +34,15 @@ var restTemplate: RestTemplate = RestTemplate()
          val model3: ShipUnit = ShipUnit()
         val model4: ContainerInShipItems = ContainerInShipItems()
         val model5 : PostEcmrEvent = PostEcmrEvent()
+        val model9: AuthCode = AuthCode()
+        val model6: ECMRPosted = ECMRPosted()
+         val model7: ECMRPostedEvent = ECMRPostedEvent()
+        val model8: ServiceResponse = ServiceResponse()
+    }
+
+    @Test
+    fun testFlag() {
+       var ecmr: MutableList<ECMR> = mutableListOf()
+        updateStatus.changeFlag(ecmr)
     }
 }
