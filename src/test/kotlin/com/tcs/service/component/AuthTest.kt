a96b4df94c76b87ffb114915451b676f50625448
prabhat2020 <kprabhat0123@outlook.com>
2021-01-29 16:29:49 +0530

    tests

--- src/test/kotlin/com/tcs/service/component/AuthTest.kt
+++ src/test/kotlin/com/tcs/service/component/AuthTest.kt
@@ -1,6 +1,11 @@
 package com.tcs.service.component
 
 import com.tcs.service.constant.URLPath
+import com.tcs.service.events.PostEcmrEvent
+import com.tcs.service.model.ContainerInShipItems
+import com.tcs.service.model.ECMR
+import com.tcs.service.model.OrderInShipment
+import com.tcs.service.model.ShipUnit
 import com.tcs.service.service.Auth
 import com.tcs.service.utility.Utility
 import org.junit.Assert
@@ -24,5 +29,13 @@ var restTemplate: RestTemplate = RestTemplate()
 
         Assert.assertNotNull(result)
 
+    }
+    @Test
+    fun testModels() {
+       val model1: ECMR = ECMR()
+        val model2: OrderInShipment = OrderInShipment()
+         val model3: ShipUnit = ShipUnit()
+        val model4: ContainerInShipItems = ContainerInShipItems()
+        val model5 : PostEcmrEvent = PostEcmrEvent()
     }
 }
